fd9840154ed1b038b604c7d4757551652055df4d
AbboDev <abbodev@gmail.com>
2022-11-03 23:48:42 +0100

    Simulate QR Code example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,45 @@
-import './index.css';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class QRCode extends React.Component {
+  render() {
+    return (
+      <div className="o-qrcode">
+        <img src={this.props.link} alt="" />
+      </div>
+    );
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="o-card">
+        <QRCode link="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=160x160&amp;format=png&amp;color=fff&amp;bgcolor=3685fe" />
+
+        <div className="o-card__text">
+          <h1>{this.props.title}</h1>
+          <p>{this.props.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="o-app">
+        <Card
+          title="Improve your front-end skills by building projects"
+          description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+        />
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
